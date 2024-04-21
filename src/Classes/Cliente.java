ccf080405bd83086de0f1de625fc4358ca4829b4
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-04-21 19:15:43 -0300

    Escopo classe Funcionario e correções na Cliente

--- src/Classes/Cliente.java
+++ src/Classes/Cliente.java
@@ -10,17 +10,27 @@ public class Cliente extends Thread{
 	}
 	
 	public void run(Loja loja1, Loja loja2) {
+		comprar(loja2, loja2);
+	}
+	
+	public void comprar(Loja loja1, Loja loja2) {
 		boolean alternar = true;
-		
+
 		while(conta.saldo != 0) {
 			int valor = new Random().nextInt(2);
 			double resultado = (valor == 0) ? 100 : 200;
+			if (this.conta.saldo == 100) {
+				resultado = 100;
+			}
 			if (alternar){
 				loja1.conta.saldo += resultado;
+				System.out.print("compra de "+resultado+" na loja "+loja1.nome+"\n");
 			}else {
 				loja2.conta.saldo += resultado;
+				System.out.print("compra de "+resultado+" na loja "+loja2.nome+"\n");
 			}
 			this.conta.saldo -= resultado;
+			System.out.print("saldo na conta do cliente "+this.nome+": "+this.conta.saldo+"\n");
 			alternar = !alternar;
 		}
 	}
--- src/Classes/Funcionario.java
+++ src/Classes/Funcionario.java
@@ -1,5 +1,23 @@
 package Classes;
 
-public class Funcionario {
-
+public class Funcionario extends Thread{
+	String nome;
+	Conta contaSalario = new Conta(0, nome);
+	Conta contaInvestimento = new Conta(0, nome);
+	
+	public Funcionario (String nome) {
+		this.nome = nome;
+	}
+	
+	public void run() {
+		investir();
+	}
+	
+	public void investir(){
+		if (this.contaSalario.saldo >= 1400) {
+			double valorAInvestir = (this.contaSalario.saldo*20)/100;
+			this.contaInvestimento.saldo += valorAInvestir;
+			this.contaSalario.saldo -= valorAInvestir;
+		}
+	}
 }
